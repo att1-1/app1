f934f99377e8072120bc59dd802f7526659a9fc7
Евгений <evgeniy.rekw@gmail.com>
2024-12-28 14:44:20 +0500

    Adding fixtures

--- main/views.py
+++ main/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 def index(request):
+    # pylint: disable=no-member
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
+        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
+
 
 
 def about(request):
