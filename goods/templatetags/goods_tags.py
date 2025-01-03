3a30a01b54770b4ef5a4114bbf6fed4e8c082879
Евгений <evgeniy.rekw@gmail.com>
2025-01-03 20:13:30 +0500

    Simple tag for categories

---
+++ goods/templatetags/goods_tags.py
@@ -0,0 +1,11 @@
+# pylint: disable=no-member
+from django import template
+
+from goods.models import Categories
+
+
+register = template.Library()
+
+@register.simple_tag()
+def tag_categories():
+    return Categories.objects.all()
